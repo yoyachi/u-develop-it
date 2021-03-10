6077ee68aa20a7d7858c211c18a9245d5eba3774
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-03-10 11:48:06 -0600

    add dependencies and create connection to Express.js server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other requests(Not Found) Catch all
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+
+// add the function that will start the Express.js server on port 3001.
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
